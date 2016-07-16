6a78d97d73cde13d4e4ad3ed061c6c5cefc12d97
marinacor1 <marinaspam1234@gmail.com>
2016-07-16 15:27:41 -0600

    Idea table created

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'ideas#index'
 
-  resouces :ideas, only: [:show]
+  resources :ideas, only: [:show]
 
   namespace :api do
     namespace :v1 do
-      resources :ideas, defaults {format: :json} do
+      resources :ideas, defaults: {format: :json} do
       end
     end
   end
-  
+
 end
