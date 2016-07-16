1d01cb21e442e03d8f4854a84539c56ce56f8190
marinacor1 <marinaspam1234@gmail.com>
2016-07-16 15:17:20 -0600

    Internal api written

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,6 +1,35 @@
-class IdeasController < Api::ApiController
+class Api::V1::IdeasController < ApplicationController
+  respond_to :json
+
   def index
-    ideas = Idea.all
-    @ideas = idea.order(created_at: :desc)
+    respond_with Idea.all
+  end
+
+  def create
+    respond_with Idea.create(idea_params)
+  end
+
+  def update
+    idea = Idea.find(params['id'])
+    if params[:changeValue]
+      idea.quality = idea.change_quality(params['changeValue'])
+      idea.save
+    end
+    if idea.update(idea_params)
+      idea.save
+      render json: {response: 'succesful', quality: idea.quality}
+    else
+      render json: {response: 'failed'}
+    end
+  end
+
+  def destroy
+    respond_with Idea.destroy(params[:id])
+  end
+
+  private
+
+  def idea_params
+    params.permit(:title, :quality, :id)
   end
 end
