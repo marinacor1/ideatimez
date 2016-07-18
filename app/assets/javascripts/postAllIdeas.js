3dbd02471bc2e0cdebf8d534d91030bf6dc6a6c8
marinacor1 <marinaspam1234@gmail.com>
2016-07-18 16:18:39 -0600

    Refactored code and added ul tags

--- app/assets/javascripts/postAllIdeas.js
+++ app/assets/javascripts/postAllIdeas.js
@@ -1,19 +1,24 @@
-function postAllIdeas(){
+function postAllIdeas(selector){
     $.ajax({
       type: "GET",
       url: "api/v1/ideas",
-      success: renderIdeas,
+      success: renderIdeas(selector),
     });
 
-function renderIdeas(response){
-  var $target = $('#idea-table');
-  $.each(response, function(index, idea){
-    $target.prepend(formatIdea(idea));
-  });
+function renderIdeas(selector){
+	return function(response) {
+      var $target = $(selector);
+      $.each(response, function(index, idea){
+       $target.prepend(formatIdea(idea));
+      });
+	};
  }
-
  function formatIdea(idea) {
-   return '<div id="idea-table">' + '<ul>Title: ' +idea.title +
-   '</ul><ul>Body: ' + idea.description + '</ul><ul>Quality: '+ idea.quality + '</ul></div>';
+   console.log ("idea is " + idea.title);
+   console.log ("body is " + idea.description);
+   console.log ("quality is " + idea.quality);
+   return '<div id="idea-table"><ul>' + 'Title:' +idea.title +
+   '</ul><ul>Body:' + idea.description + '</ul><ul>Quality: '+ idea.quality + '</ul></div>';
  }
 }
+postAllIdeas('#idea-table');
