f523a3cb225221ee3cd3b639f0ee7a7b183e5d90
marinacor1 <marinaspam1234@gmail.com>
2016-07-19 13:01:42 -0600

    Search box loads at beginning

--- app/assets/javascripts/ideaForm.js
+++ app/assets/javascripts/ideaForm.js
@@ -1,6 +1,7 @@
  function ideaForm(selector){
   var $target = $(selector);
   $target.append(formatForm());
+  $target.prepend(searchBox());
 }
 
   function formatForm(){
@@ -9,4 +10,8 @@
     '<input id="idea-t" type="text" name="ideatitle" class="idea-title"><br>' +
   'Idea Body: <br> ' + '<input type"text" id="idea-b" class="idea-body" name="ideatitle">' +
   '<input id="submit-button" type="submit" value="Save"> </form></div>';
+}
+
+function searchBox(){
+  return 'Search: <input type="text" name="searchbox" value="Search ideas"><br><br>'
 }
